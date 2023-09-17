8bfeab2702bf4131707262e79d75f11da5ecef19
hola0511 <espinosaariassamuel@gmail.com>
2023-09-17 01:16:08 -0500

    la app esta igual que el tutorial

--- froend/src/App.js
+++ froend/src/App.js
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react'; // Agrega la importación de useState
+
 import logo from './logo.svg';
 import './App.css';
-import Component from './componente/MiComponente';
-import myApp from './componente/app';
+import { Header } from './components/Header';
+import { ProductList } from './components/ProductoList';
 
 function App() {
+  const [allProducts, setAllProducts] = useState([]);
+  const [total, setTotal] = useState(0);
+  const [countProducts, setCountProducts] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <myApp/>
-      </header>
-    </div>
+    <>
+      <Header
+        allProducts={allProducts}
+        setAllProducts={setAllProducts}
+        total={total}
+        setTotal={setTotal}
+        countProducts={countProducts}
+        setCountProducts={setCountProducts}
+      />
+      <ProductList
+        allProducts={allProducts}
+        setAllProducts={setAllProducts}
+        total={total}
+        setTotal={setTotal}
+        countProducts={countProducts}
+        setCountProducts={setCountProducts}
+      />
+    </>
   );
 }
 
