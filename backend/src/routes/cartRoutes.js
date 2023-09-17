2129535449392e19b37551736780af0f29f3fa85
JpRoGarcia <juan.1020405@gmail.com>
2023-09-17 00:44:30 -0500

    arreglo cart

--- backend/src/routes/cartRoutes.js
+++ backend/src/routes/cartRoutes.js
@@ -4,11 +4,11 @@ const router = express.Router();
 const cartController = require('../controllers/cartController');
 
 // Agregar un producto al carrito
-cartRouter.post('/add', cartController.addToCart);
+router.post('/add', cartController.addToCart);
 
 // Eliminar un producto del carrito por SKU
-cartRouter.delete('/remove/:sku', cartController.removeFromCart);
+router.delete('/remove/:sku', cartController.removeFromCart);
 
 // Obtener el contenido del carrito y su precio total
-cartRouter.get('/contents', cartController.getCartContents);
+router.get('/contents', cartController.getCartContents);
 module.exports = router;
